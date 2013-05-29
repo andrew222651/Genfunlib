2afdf447ca27a99b73920eb99d56b58704db89ca
Andrew MacFie <andrewmacfie@gmail.com>
2013-05-29 14:26:07 -0400

    working on coef extraction

--- GFeqn2coefs.m
+++ GFeqn2coefs.m
@@ -2,6 +2,9 @@
 
 BeginPackage["Genfunlib`GFeqn2coefs`"]
 
+CoefsByDerivs::usage = "";
+CoefsByNewton::usage = "";
+
 Begin["`Private`"] (* Begin Private Context *) 
 
 coefs[eqn_, n_, func_, indet_] := 
@@ -19,6 +22,23 @@ coefs[eqn_, n_, func_, indet_] :=
   {k, 0, n}];
  (Derivative[#][renamedFunc][0]/#! &) /@ Range[0, n]
  ]; 
+
+(* http://www.mathematik.uni-kassel.de/~koepf/Publikationen/Koepf-\
+Newton2008.pdf *)
+
+fastImplicitTaylor[
+   lhs_ == rhs_, x_[t_], x0_, n_
+   ] := Module[{
+    f = lhs - rhs, ff, approx, z, k, 
+    m = -1 + 2^\[LeftCeiling]Log[2, n]\[RightCeiling]
+    },
+   ff[z_] := Normal[(f /. x[t] :> z) + O[t]^(m + 1)];
+   approx = x0;
+   Do[
+    approx = Normal[# - ff[#]/ff'[#] &[approx] + O[t]^(2^k)], {k, 1, 
+     Log[2, m + 1]}];
+   approx + O[t]^(n + 1)
+   ];
 
 End[] (* End Private Context *)
 
