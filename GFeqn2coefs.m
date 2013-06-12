f4739c78b2fe728ed4509330ec00d078b93ebcb0
Andrew MacFie <andrewmacfie@gmail.com>
2013-06-11 21:12:21 -0400

    series expansion

--- GFeqn2coefs.m
+++ GFeqn2coefs.m
@@ -2,42 +2,76 @@
 
 BeginPackage["Genfunlib`GFeqn2coefs`"]
 
-CoefsByDerivs::usage = "";
-CoefsByNewton::usage = "";
+CoefsByDerivs::usage = "CoefsByDerivs[{eqn1, eqn2,...}, {f,g, ...}, " <>
+	"{x, 0, nx}, {y, 0, ny}, ...] gives the Maclaurin expansions of the " <>
+	"power series f,g,... satisfying the equations.";
+CoefsByNewton::usage = "CoefsByNewton[eqn, f, {x, 0, nx}] gives the " <>
+	"Maclaurin expansion in x of f satisfying eqn.";
 
 Begin["`Private`"] (* Begin Private Context *) 
 
-coefs[eqn_, n_, func_, indet_] := 
-     Module[
- {
-  renamedFunc,
-  renamedEqn , k
-  },
- renamedEqn = eqn /. func -> renamedFunc;
- Do[
-  Derivative[k][renamedFunc][0] = 
-   First@(Derivative[k][renamedFunc][0] /. 
-      Solve[renamedEqn /. indet -> 0, Derivative[k][renamedFunc][0]]);
-  renamedEqn = D[renamedEqn, indet],
-  {k, 0, n}];
- (Derivative[#][renamedFunc][0]/#! &) /@ Range[0, n]
- ]; 
+(* what can usually be used as a variable, according to 
+	ref/message/General/ivar *)
+variablePattern = Except[_String | _?NumberQ | _Plus | _Times | 
+	_Sum | _Product | _^_Integer];
 
-(* http://www.mathematik.uni-kassel.de/~koepf/Publikationen/Koepf-\
-Newton2008.pdf *)
+CoefsByDerivs::invalid = "Invalid input.";
+CoefsByNewton::invalid = "Invalid input.";
 
-fastImplicitTaylor[
-   lhs_ == rhs_, x_[t_], x0_, n_
-   ] := Module[{
-    f = lhs - rhs, ff, approx, z, k, 
-    m = -1 + 2^\[LeftCeiling]Log[2, n]\[RightCeiling]
+validateSystem[system : {(_ == _)..}, series: { variablePattern[__].. }, 
+	iters:({variablePattern, 0, _Integer?NonNegative}..)] := Module[
+	{
+		ok = True
+	},
+	If[ !ok, Message[CoefsByDerivs::invalid]];
+	ok
+];
+
+validateSystem[___] := (Message[CoefsByDerivs::invalid];False);
+
+validateEquation[lhs_ == rhs_, series:variablePattern, 
+	{var:variablePattern, 0, _Integer?NonNegative}] := Module[
+	{ok = True},
+
+	ok = ok && !FreeQ[series, var] && !FreeQ[lhs-rhs, series];
+	ok = ok && D[lhs - rhs, series] =!= 0;
+
+	If[ !ok, Message[CoefsByNewton::invalid]];
+	ok
+];
+
+validateEquation[___] := (Message[CoefsByNewton::invalid];False);
+
+CoefsByDerivs[x:Except[_List], y__] := CoefsByDerivs[{x}, y];
+CoefsByDerivs[x_, y:Except[_List], z__] := CoefsByDerivs[x, {y}, z];
+
+CoefsByDerivs[system_, series_, iters__] := Module[
+   {
+    indets = {iters} /. {x_, 0, _} :> x},
+    systemCanon = system /. (l_ == r_ :> l - r),
+    expansion,
+    eqns, subs
     },
-   ff[z_] := Normal[(f /. x[t] :> z) + O[t]^(m + 1)];
-   approx = x0;
+   (
+     expansion = Normal[Series[systemCanon, iters]];
+     eqns = Thread[Flatten[CoefficientList[expansion, indets]] == 0];
+     subs = Flatten[Solve[eqns]];
+     Thread[series == (Series[series, iters] /. subs)]
+     ) /; validateSystem[system, series, iters]
+   ];
+
+CoefsByNewton[lhs_ == rhs_, series_, {var_, 0, n_}] := Module[
+   {
+    f = lhs - rhs, ff, approx = 0, m = 2^Ceiling[Log[2, n]]
+   },
+   (
+   ff[z_] := Normal[(f /. series :> z) + O[var]^m];
    Do[
-    approx = Normal[# - ff[#]/ff'[#] &[approx] + O[t]^(2^k)], {k, 1, 
-     Log[2, m + 1]}];
-   approx + O[t]^(n + 1)
+    approx = Normal[# - ff[#]/ff'[#] &[approx] + O[var]^(2^k)], 
+    {k, 1, Log[2, m]}
+   ];
+   approx + O[var]^(n + 1)
+   )/; validateEquation[lhs==rhs, series, {var, 0, n}] 
    ];
 
 End[] (* End Private Context *)
--- Util.m
+++ Util.m
@@ -2,19 +2,21 @@
 
 BeginPackage["Genfunlib`Util`"]
 
-egf2ogf::usage = "efg2ogf[f, z] gives the OGF form of the EGF f in the indeterminate z";
-ogf2egf::usage = "ofg2egf[f, z] gives the EGF form of the OGF f in the indeterminate z";
+Egf2ogf::usage = "efg2ogf[f, z] gives the OGF form of the EGF f in the " <>
+"indeterminate z";
+Ogf2egf::usage = "ofg2egf[f, z] gives the EGF form of the OGF f in the " <>
+" indeterminate z";
 
 Begin["`Private`"] 
 
-egf2ogf[egf_, indet_] := Module[
+Egf2ogf[egf_, indet_] := Module[
    {temp},
    (LaplaceTransform[egf, indet, 1/temp]/temp // Simplify) /. 
     temp -> indet
    ];
 
 (* egf2ogf = ogf2egf^-1 *)
-ogf2egf[ogf_, indet_] := Module[
+Ogf2egf[ogf_, indet_] := Module[
    {temp1, temp2}, 
    InverseLaplaceTransform[(ogf /. {indet -> 1/temp1})/temp1, temp1, 
      temp2] /. temp2 -> indet
